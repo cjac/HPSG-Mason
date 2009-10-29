3439a07a45e6f6af1f46970aa15a8c94604ad3c0
C.J. Adams-Collier <cjac@colliertech.org>
2009-10-29 00:16:07 +0000

    Import of CJCOLLIER/HPSG-Mason-0.03 from CPAN.
    gitpan-cpan-distribution: HPSG-Mason
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         CJCOLLIER/HPSG/HPSG-Mason-0.03.tar.gz
    gitpan-cpan-author:       CJCOLLIER
    gitpan-cpan-maturity:     released

--- lib/HPSG/Mason.pm
+++ lib/HPSG/Mason.pm
@@ -7,11 +7,11 @@ HPSG::Mason - Mason components for rendering Head-driven Phrase Structure Gramma
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
